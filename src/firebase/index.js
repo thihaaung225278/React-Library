776f62bdc67839b7cc0769578b9e224e94bfc539
thihaaungclickr <thiha@clickrmedia.com>
2025-08-25 23:46:07 +0630

    setup firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA8vo1tHAO2gutzDR-rrwQsCFVe51Izlzw",
+    authDomain: "library-af82d.firebaseapp.com",
+    projectId: "library-af82d",
+    storageBucket: "library-af82d.firebasestorage.app",
+    messagingSenderId: "861421980366",
+    appId: "1:861421980366:web:8b3550a7eb6635aa3f9e9f",
+    measurementId: "G-SZ0S0VWM9P"
+};
+const app = initializeApp(firebaseConfig);
